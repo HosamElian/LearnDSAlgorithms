fc165a2fb05fc4c2fec737152fa37eadb9de7e75
HosamElian <Hossam.Elian@cybral.com>
2026-05-24 03:06:28 +0300

    Add Graph

---
+++ Graphs/Graph.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearnDSAlgorithms.Graphs
+{
+	public class Graph
+	{
+		int _verticesCount;
+		int[,] adjMat;
+
+		public Graph(int n)
+		{
+			_verticesCount = n;
+			adjMat = new int[n, n];
+		}
+
+		public void InsertEdge(int fromVertex, int toVertex, int cost = 1)
+		{
+			adjMat[fromVertex, toVertex] = cost;
+		}
+		public int RemoveEdge(int fromVertex, int toVertex)
+		{
+			return adjMat[fromVertex, toVertex] = 0;
+		}
+		public bool ExistEdge(int fromVertex, int toVertex)
+		{
+			return adjMat[fromVertex, toVertex] != 0;
+		}
+		public int VertexCount()
+		{
+			return _verticesCount;
+		}
+		public int EdgeCount()
+		{
+			int count = 0;
+			for (int i = 0; i < _verticesCount; i++)
+			{
+				for (int j = 0; j < _verticesCount; j++)
+				{
+					if (adjMat[i, j] != 0)
+						count++;
+				}
+			}
+			return count;
+		}
+		public void GetEdges()
+		{
+			for (int i = 0; i < _verticesCount; i++)
+			{
+				for (int j = 0; j < _verticesCount; j++)
+				{
+					if (adjMat[i, j] != 0)
+						Console.WriteLine($"{i} -- {j}");
+				}
+			}
+		}
+		public int OutDegree(int vertex)
+		{
+			int count = 0;
+			for (int i = 0; i < _verticesCount; i++)
+			{
+				if (adjMat[vertex, i] != 0)
+				{
+					count++;
+				}
+			}
+			return count;
+		}
+		public int InDegree(int vertex)
+		{
+			int count = 0;
+			for (int i = 0; i < _verticesCount; i++)
+			{
+				if (adjMat[i, vertex] != 0)
+				{
+					count++;
+				}
+			}
+			return count;
+		}
+		public void Display()
+		{
+			for (int i = 0; i < _verticesCount; i++)
+			{
+				for (int j = 0; j < _verticesCount; j++)
+					Console.Write(adjMat[i, j] + "\t");
+				Console.WriteLine();
+			}
+		}
+
+		public void BreadthFirstSearch(int root)
+		{
+			if (root > _verticesCount)
+			{
+				Console.WriteLine("Node has no neighbor node");
+				return;
+			}
+			Queue<int> queue = new Queue<int>();
+			var visited = new bool[_verticesCount];
+			visited[root] = true;
+			queue.Enqueue(root);
+			Console.Write(root + " ");
+			while (queue.Count > 0)
+			{
+				int node = queue.Dequeue();
+				for (int neighbor = 0; neighbor < _verticesCount; neighbor++)
+				{
+					if (adjMat[node, neighbor] == 1 && !visited[neighbor])
+					{
+						Console.Write(neighbor + " ");
+						queue.Enqueue(neighbor);
+						visited[neighbor] = true;
+					}
+				}
+			}
+		}
+		public void DepthFirstSearch(int node)
+		{
+			if (node > _verticesCount)
+			{
+				Console.WriteLine("Node has no neighbor node");
+				return;
+			}
+			var visited = new bool[_verticesCount];
+			depthFirstSearch(node, visited);
+		}
+		private void depthFirstSearch(int node, bool[] visited)
+		{
+			Console.Write(node + " ");
+			visited[node] = true;
+			for (int neighbor = 0; neighbor < _verticesCount; neighbor++)
+				if (adjMat[node, neighbor] == 1 && !visited[neighbor])
+					depthFirstSearch(neighbor, visited);
+		}
+
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,22 +1,69 @@
-﻿using LearnDSAlgorithms.SortAlgorithms;
+﻿using LearnDSAlgorithms.Graphs;
 using System;
 
 namespace LearnDSAlgorithms
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int[] numbers = { 42, 32, 33, 52, 37, 47, 51 };
-            Console.WriteLine("Original array:");
-            Console.WriteLine(string.Join(", ", numbers));
-            var sort = new RadixSort();
-            var sortedList = sort.Sort(numbers);
-            foreach ( var item in sortedList )
-            {
-                Console.WriteLine(item);
-            }
-            Console.ReadKey();
-        }
-    }
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Graph g = new Graph(4);
+			Console.WriteLine("Graphs Adjacency Matrix:");
+			g.Display();
+			Console.WriteLine("Vertices: " + g.VertexCount());
+			Console.WriteLine("Edges Count: " + g.EdgeCount());
+			g.InsertEdge(0, 1, 1);
+			g.InsertEdge(0, 2, 1);
+			g.InsertEdge(1, 0, 1);
+			g.InsertEdge(1, 2, 1);
+			g.InsertEdge(2, 0, 1);
+			g.InsertEdge(2, 1, 1);
+			g.InsertEdge(2, 3, 1);
+			g.InsertEdge(3, 2, 1);
+			Console.WriteLine("Graphs Adjacency Matrix:");
+			g.Display();
+			Console.WriteLine("Vertices: " + g.VertexCount());
+			Console.WriteLine("Edges Count: " + g.EdgeCount());
+			g.GetEdges();
+			Console.WriteLine("Is Edge between 1--3: " + g.ExistEdge(1, 3));
+			Console.WriteLine("Is Edge between 1--2: " + g.ExistEdge(1, 2));
+			Console.WriteLine("Degree of Vertex 2: " + g.InDegree(2));
+			Console.WriteLine("Remove Edge between 1--2");
+			g.RemoveEdge(1, 2);
+			Console.WriteLine("Graphs Adjacency Matrix:");
+			g.Display();
+			Console.WriteLine("Is Edge between 1--2: " + g.ExistEdge(1, 2));
+			Console.WriteLine("BreadthFirstSearch: ");
+			g.BreadthFirstSearch(0);
+			Console.WriteLine();
+			Console.WriteLine("DepthFirstSearch: ");
+			g.DepthFirstSearch(0);
+			Console.WriteLine();
+			Console.WriteLine("####### another graph #########");
+			Graph g2 = new Graph(7);
+			g2.InsertEdge(0, 1, 1);
+			g2.InsertEdge(0, 5, 1);
+			g2.InsertEdge(0, 6, 1);
+			g2.InsertEdge(1, 0, 1);
+			g2.InsertEdge(1, 2, 1);
+			g2.InsertEdge(1, 5, 1);
+			g2.InsertEdge(1, 6, 1);
+			g2.InsertEdge(2, 3, 1);
+			g2.InsertEdge(2, 4, 1);
+			g2.InsertEdge(2, 6, 1);
+			g2.InsertEdge(3, 4, 1);
+			g2.InsertEdge(4, 2, 1);
+			g2.InsertEdge(4, 5, 1);
+			g2.InsertEdge(5, 2, 1);
+			g2.InsertEdge(5, 3, 1);
+			g2.InsertEdge(6, 3, 1);
+			Console.WriteLine("BreadthFirstSearch: ");
+			g2.BreadthFirstSearch(0);
+			Console.WriteLine();
+			Console.WriteLine("DepthFirstSearch: ");
+			g2.DepthFirstSearch(0);
+			Console.WriteLine();
+			Console.ReadKey();
+		}
+	}
 }
